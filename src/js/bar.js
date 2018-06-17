485e123f221de2921ca93f99db16753e8647811b
william-abboud <william_y_abboud@abv.bg>
2018-06-18 01:36:21 +0300

    Removed old examples in favor or new react example

--- src/js/bar.js
+++
@@ -1,9 +0,0 @@
-function createH2(message) {
-  const h2 = document.createElement('h2');
-  h2.innerHTML = message;
-
-  return h2;
-}
-
-
-export default createH2;
--- src/js/hello-world.js
+++
@@ -1,6 +0,0 @@
-import createH2 from './bar';
-
-const message = "Hello World";
-export const h2 = createH2(message);
-
-export default message;
